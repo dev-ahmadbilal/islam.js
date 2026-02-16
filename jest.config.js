cbeded4602a6b5ca631c33d43dcfaeb5faa935aa
Ahmad Bilal <143768499+dev-ahmadbilal@users.noreply.github.com>
2026-02-16 11:49:43 +0500

    Revert "Feat/package size"

--- jest.config.js
+++ jest.config.js
@@ -3,11 +3,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/test/**/*.spec.ts'],
-  collectCoverageFrom: [
-    '<rootDir>/src/**/*.ts',
-    '!<rootDir>/src/types/**/*.ts',
-    '<rootDir>/scripts/**/*.js',
-  ],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/types/**/*.ts'],
   transform: {
     '^.+\\.tsx?$': [
       'ts-jest',
